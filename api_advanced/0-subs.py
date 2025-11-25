17ecfe9d05db2f1b06513d38f4d60c832a62cbc7
Eelaf Abuelgasim Abdalla Adam <e.adam@alustudent.com>
2025-11-25 11:33:53 +0200

    Update 0-subs.py

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+0-subs
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    number_of_subscribers
+    """
+    url = "https://oauth.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My User Agent 1.0"}
+
+    response = requests.get(
+                url,
+                headers=headers,
+                allow_redirects=False
+                )
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
